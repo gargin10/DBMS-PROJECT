11da39a667cfbc5dfd95171aa684c7594d724791
gargin10 <82716899+gargin10@users.noreply.github.com>
2023-04-22 12:31:04 +0530

    Update firstimport.php

--- railres/firstimport.php
+++ railres/firstimport.php
@@ -2,7 +2,7 @@
 $host="localhost"; // Host name
 $username="root"; // Mysql username
 $password=""; // Mysql password
-$db_name="raildraft"; // Database name
+$db_name="railres"; // Database name
 
 $conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
 
