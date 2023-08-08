e26e4e5e7738ebb854fa4b859eb9d5bacc6bd9a1
chaitanya3717 <chaitanya20@iitk.ac.in>
2023-08-08 17:17:38 +0530

    adding all files of final project

---
+++ README.md
@@ -0,0 +1,22 @@
+Railway Booking app -
+by
+Abhishek Bansod (200022)
+Anushka Panda (200174)
+Gargi Naladkar (200371)
+Sana (200599)
+Shreya Kacholia (180735)
+
+#Front end: HTML, CSS, JavaScript
+#Back end: PHP, MySQL
+#PHP: Hypertext Preprocessor (PHP) is a technology that allows software developers to create
+#dynamically generated web pages, in HTML, XML, or other document types, as per client request. PHP is open source software.
+#MySQL: MySql is a database, widely used for accessing querying, updating, and managing data in databases.
+#Software Requirement(any one)
+#XAMPP Server
+
+Following are the steps to configure the setup:Install “xampp”
+Extract the .zip file and add the folder inside “C:\xampp\htdocs”.
+Start xampp (MySQL)
+Open the MySQL admin and create a new database “ralidraft” and import “railres.sql” from the folder.
+Please note that the database name should be same as it is in the firstimport.php file.
+Run “http://www.localhost/railres/” and we are ready to use -
---
+++ index1.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Railway Booking </title>
+	<link rel="shortcut icon" href="images/train (3).png"></link>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<link href="css/bootstrap.min.css" rel="stylesheet" ></link>
+	<link href="css/bootstrap.css" rel="stylesheet" ></link>
+	<link href="css/Default.css" rel="stylesheet" >	</link>
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script>
+		$(document).ready(function()
+		{
+			var x=(($(window).width())-1024)/2;
+			$('.wrap').css("left",x+"px");
+		});
+	</script>
+	
+	<!-- <script type="text/javascript" src="js/bootstrap.min.js"></script> -->
+	<!-- <script type="text/javascript" src="js/bootstrap.js"></script> -->
+	<script type="text/javascript" src="js/man.js"></script>
+	
+	
+</head>
+<body>
+
+	<div class="wrap">
+		<!-- Header -->
+		<div class="header">
+			<div style="float:left;width:150px;">
+				<img src="images/rlogo.png"/>
+			</div>		
+			<div>
+			<div style="float:right; font-size:20px;margin-top:20px;">
+			<?php
+			 if(isset($_SESSION['name']))	
+			 {
+			 echo "Welcome,".$_SESSION['name']."&nbsp;&nbsp;&nbsp;<a href=\"logout.php\" class=\"btn btn-info\">Logout</a>";
+			 }
+			 else
+			 {
+			 ?>
+				<a href="login1.php" class="btn btn-info">Login</a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="signup.php?value=0" class="btn btn-info">Signup</a>
+			<?php } ?>
+			
+			
+			</div>
+			<div id="heading">
+				<a href="index.php">Railway Booking </a>
+			</div>
+			</div>
+		</div>
+		
+		<!-- Navigation bar -->
+		<div class="navbar navbar-inverse">
+			<div class="navbar-inner">
+				<div class="container" >
+				<a class="brand" href="index1.php" >HOME</a>
+				<a class="brand" href="addtrain.php">ADD TRAIN</a>
+				<a class="brand" href="findtrain.php">FIND TRAIN</a>
+				<a class="brand" href="schedule.php">SCHEDULE TRAIN</a>
+				<a class="brand" href="interlist.php">ADD INTERLIST</a>
+				</div>
+			</div>
+		</div>
+		<div class="span12 well">
+			<div  style="width:800px; height:400px; float:left; margin-bottom:3px;">
+			    <div style="float:center; width:100%;">
+				     <img src="images/train.jpg" width="1000" height="300" >
+			    </div>		
+			</div>
+		</div>
+		
+<footer >
+		<div style="width:100%;">
+			<div style="float:right;">
+			<p class="text-right text-info"> DBMS PROJECT </p>
+			</div>
+		</div>
+</footer>	
+	
+</body>
+</html>
+
+<?php
+
+if(isset($_SESSION['error']))
+{
+session_destroy();
+}
+
+?>
